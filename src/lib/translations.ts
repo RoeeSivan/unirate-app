1f75ec31847903ccc5e8121a3034eb06760fa293
Roee Sivan <roee.sivan@post.runi.ac.il>
2026-03-10 16:02:18 +0200

    mandatory

--- src/lib/translations.ts
+++ src/lib/translations.ts
@@ -101,7 +101,7 @@ const translations = {
   year1: { en: 'Year 1', he: 'שנה א\'' },
   year2: { en: 'Year 2', he: 'שנה ב\'' },
   year3: { en: 'Year 3', he: 'שנה ג\'' },
-  entrepreneurshipCourses: { en: 'Entrepreneurship', he: 'יזמות' },
+  entrepreneurshipCourses: { en: 'Entrepreneurship mandatory', he: 'יזמות חובה' },
   electives: { en: 'Electives', he: 'בחירה' },
   verticals: { en: 'Verticals', he: 'וורטיקלים' },
   vertical: { en: 'Vertical', he: 'וורטיקל' },
